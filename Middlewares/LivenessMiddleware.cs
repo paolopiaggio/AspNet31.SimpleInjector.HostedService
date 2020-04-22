7b3c7efb4b7a905967c5361505cecdde88241944
Paolo Piaggesi <paolo.piaggesi@deltatre.com>
2020-04-22 21:38:31 +0200

    aspnetcore3.1 web api

---
+++ Middlewares/LivenessMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNet31.SimpleInjector.HostedService.Middlewares
+{
+    public class LivenessMiddleware
+    {
+        private const string LivenessPathValue = "/ping";
+
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (context.Request.Path == LivenessPathValue)
+            {
+                return context.Response.WriteAsync("pong", Encoding.UTF8);
+            }
+
+            return next.Invoke(context);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+﻿using AspNet31.SimpleInjector.HostedService.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SimpleInjector;
+
+namespace AspNet31.SimpleInjector.HostedService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _container.Options.ResolveUnregisteredConcreteTypes = true;
+        }
+
+        public IConfiguration Configuration { get; }
+        private readonly Container _container = new Container();
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+            services.AddControllers();
+            services.AddSimpleInjector(_container, options =>
+            {
+                options.AddAspNetCore();
+                options.AddLogging();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            app.UseSimpleInjector(_container);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseMiddleware<LivenessMiddleware>(_container);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            _container.Verify();
+        }
+    }
+}
